293af54dbcbe5bf2dbd38b05db6315924d124ae2
Daniel <daniel.gallagher.js@gmail.com>
2025-12-16 11:16:28 +0000

    fixed syntax error

--- src/reef/obfuscators/replace.py
+++ src/reef/obfuscators/replace.py
@@ -1,28 +1,52 @@
 from reef.obfuscators.base import Obfuscator
-from reef.obfuscators.spacy_registry import get_spacy_nlp
+from nltk.tokenize.treebank import TreebankWordDetokenizer, TreebankWordTokenizer
+from typing import Literal
 
 
 class ReplaceObfuscator(Obfuscator):
 
-    def obfuscate(self, text: str, algorithm="nouns-only") -> str:
-        return self._to_nouns(text)
+    POS = Literal["NOUN","PROPN"]
+    Algorithm = Literal["nouns-only", "nouns-and-prop-only",
+                        "no-nouns", "no-nouns-or-prop"]
 
-    def _keep_only(self, text: str, pos_tags: list[str]) -> str:
-        nlp = self.spacy_nlp("ner")
-        doc = nlp(text)
+    def obfuscate(self, text: str, algorithm:Algorithm="nouns-only") -> str:
+        self.nlp = self.spacy_nlp("ner")
+        if algorithm == "nouns-only":
+            return self._nouns_only(text)
+        elif algorithm == "nouns-and-prop-only":
+            return self._nouns_only(text)
+        elif algorithm == "no-nouns":
+            return self._no_nouns(text)
+        elif algorithm == "no-nouns-or-prop":
+            return self._no_nouns_or_propn(text)
+
+    def _nouns_only(self, text: str) -> str:
+        return self._keep_only(text, ["NOUN"])
+
+    def _nouns_and_prop_only(self, text: str) -> str:
+        return self._keep_only(text, ["NOUN", "PROPN"])
+
+    def _no_nouns(self, text: str) -> str:
+        return self._keep_all_except(text, ["NOUN"])
+
+    def _no_nouns_or_propn(self, text: str) -> str:
+        return self._keep_all_except(text, ["NOUN", "PROPN"])
+
+    def _keep_only(self, text: str, pos_tags: list[POS]) -> str:
+        doc = self.nlp(text)
         remaining_tokens = []
         for token in doc:
             is_valid_pos = token.pos_ in pos_tags
             if is_valid_pos:
                 remaining_tokens.append(token.text)
-        return " ".join(remaining_tokens)
+        return TreebankWordDetokenizer().detokenize(remaining_tokens)
 
-
-    def _nouns_only(self, text: str) -> str:
-        doc = nlp(text)
-        nouns = []
+    def _keep_all_except(self, text: str, pos_tags: list[POS]) -> str:
+        doc = self.nlp(text)
+        remaining_tokens = []
         for token in doc:
-            is_noun = token.pos_ == "NOUN" or token.pos_ == "PROPN"
-            if is_noun:
-                nouns.append(token.text)
-        return " ".join(nouns)
+            is_to_be_removed = token.pos_ in pos_tags
+            if not is_to_be_removed:
+                remaining_tokens.append(token.text)
+        return TreebankWordDetokenizer().detokenize(remaining_tokens)
+
