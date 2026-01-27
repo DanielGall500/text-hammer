3eaf26c3a8d96355ef51b01841067b6039fdd37e
gall <daniel.gallagher.js@gmail.com>
2026-01-27 15:22:19 +0100

    implemented word-level

--- src/tmallet/surprisal/analysis.py
+++ src/tmallet/surprisal/analysis.py
@@ -14,8 +14,8 @@ class SurprisalAnalyser:
     def get_median(self, texts: Union[List[str], str]):
         return self.calculator.get_average_surprisal(texts, average_type="median")
 
-    def get_distribution(
-        self, texts: Union[List[str], str], save_to: Optional[str] = None
+    def get_surprisal_by_token(
+        self, texts: Union[List[str], str]
     ):
         if type(texts) is List[str]:
             processed_texts = self.calculator.calculate_surprisal_batch(texts)
@@ -23,12 +23,43 @@ class SurprisalAnalyser:
             processed_texts = self.calculator.calculate_surprisal_batch([texts])
         else:
             raise ValueError("Please provide a string or list of strings.")
+        return processed_texts
 
-        surprisals = [text["surprisals"] for text in processed_texts]
-        self.visualiser.prepare_data(
-            surprisals=surprisals,
-        )
-        density = self.visualiser.plot_density(show_median=True)
-        if save_to:
-            density.save(save_to)
-        return density
+    def get_surprisal_by_word(
+        self, texts: Union[List[str], str]
+    ):
+        if type(texts) is List[str]:
+            processed_texts = None
+        elif type(texts) is str:
+            processed_texts = self.calculator.calculate_word_level_surprisal(texts)
+        else:
+            raise ValueError("Please provide a string or list of strings.")
+        return processed_texts
+
+    def get_distribution_surprisal_by_token(
+        self, texts: Union[List[str], str], plot_to: Optional[str] = None
+    ):
+        processed_texts = self.calculator.calculate_surprisal(texts)
+
+        if plot_to:
+            surprisals = [text["surprisals"] for text in processed_texts]
+            self.visualiser.prepare_data(
+                surprisals=surprisals,
+            )
+            density = self.visualiser.plot_density(show_median=True)
+            density.save(plot_to)
+        return processed_texts
+
+    def get_distribution_surprisal_by_word(
+        self, texts: Union[List[str], str], plot_to: Optional[str] = None
+    ):
+        processed_texts = self.get_surprisal_by_word(texts)
+
+        if plot_to:
+            surprisals = [text["surprisals"] for text in processed_texts]
+            self.visualiser.prepare_data(
+                surprisals=surprisals,
+            )
+            density = self.visualiser.plot_density(show_median=True)
+            density.save(plot_to)
+        return processed_texts
